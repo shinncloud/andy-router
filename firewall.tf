7eb03683eb125a0de9bf44f006ac313055faca86
Andy Shinn <andys@andyshinn.as>
2023-05-04 16:06:58 -0500

    move caddy to miniboy

--- firewall.tf
+++ firewall.tf
@@ -1,8 +1,12 @@
+locals {
+  ip_miniboy = "192.168.1.105"
+}
+
 module "beammp" {
   source     = "./modules/port-forward"
   ports      = [30814]
   protocols  = ["tcp", "udp"]
-  to_address = "192.168.1.105"
+  to_address = local.ip_miniboy
   comment    = "beammp"
 }
 
@@ -10,6 +14,14 @@ module "beammp_http" {
   source     = "./modules/port-forward"
   ports      = [8086]
   protocols  = ["tcp"]
-  to_address = "192.168.1.105"
+  to_address = local.ip_miniboy
   comment    = "beammp http"
+}
+
+module "caddy_http" {
+  source     = "./modules/port-forward"
+  ports      = [443]
+  protocols  = ["tcp"]
+  to_address = local.ip_miniboy
+  comment    = "caddy http"
 }
